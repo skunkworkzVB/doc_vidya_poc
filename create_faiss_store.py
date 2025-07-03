b64b7516eaf79dd25911bca93f41e46e65c7701b
akshy <akshy@valuebound.com>
2025-07-03 11:29:03 +0530

    chore: update .gitignore to include new directories and files; enhance create_faiss_store.py with additional article links; update binary FAISS index files

--- create_faiss_store.py
+++ create_faiss_store.py
@@ -14,7 +14,13 @@ pages = [
     {"text_path": "assets/data/article_3.txt", "url": "https://www.docvidya.com/ketorol-dt-smiles-safe-hands-evaluating-dental-treatments-under-general-anesthesia-special-needs-and"},
     {"text_path": "assets/data/article_4.txt", "url": "https://www.docvidya.com/ketorol-dt-unlocking-pain-puzzle-key-predictors-discomfort-after-wisdom-tooth-extraction"},
     {"text_path": "assets/data/article_5.txt", "url": "https://www.docvidya.com/ketorol-dt-laser-your-pain-away-exploring-benefits-low-level-laser-therapy-root-canal-treatment"},
-    {"text_path": "assets/data/article_6.txt", "url": "https://www.docvidya.com/ketorol-dt-ache-relief-understanding-pain-levels-during-and-after-root-canal-treatment"}
+    {"text_path": "assets/data/article_6.txt", "url": "https://www.docvidya.com/ketorol-dt-ache-relief-understanding-pain-levels-during-and-after-root-canal-treatment"},
+    {"text_path": "assets/data/article_7.txt", "url": "https://www.docvidya.com/Bro-zedex-LS-parent-child-unraveling-genetic-puzzle-chronic-cough?specialities=ent"},
+    {"text_path": "assets/data/article_8.txt", "url": "https://www.docvidya.com/Bro-zedex-LS-safe-use-otc-cold-medications-children-risks-and-guidelines?specialities=ent"},
+    {"text_path": "assets/data/article_9.txt", "url": "https://www.docvidya.com/Bro-zedex-LS-science-behind-levosalbutamol-precision-approach-respiratory-care?specialities=ent"},
+    {"text_path": "assets/data/article_10.txt", "url": "https://www.docvidya.com/node/5606?specialities=ent"},
+    {"text_path": "assets/data/article_11.txt", "url": "https://www.docvidya.com/node/5581?specialities=ent"},
+    {"text_path": "assets/data/article_12.txt", "url": "https://www.docvidya.com/medshorts/oral-quadruple-therapy-offers-effective-safe-alternative-injectables-type-2-diabetes?specialities=ent"}
 ]
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=500)
